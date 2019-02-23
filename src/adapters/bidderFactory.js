bc48e5e9c80d99cef7b3a01c5c8dc786f53f143e
Manasi Moghe <manasi.moghe@pubmatic.com>
2019-02-23 22:39:11 +0530

    updated code as per latest prebid version to support syncsPerBidder parameter

--- src/adapters/bidderFactory.js
+++ src/adapters/bidderFactory.js
@@ -194,12 +194,6 @@ export function newBidder(spec) {
         events.emit(CONSTANTS.EVENTS.BIDDER_DONE, bidderRequest);
 
         registerSyncs(responses, bidderRequest.gdprConsent);
-        if (window['owpbjs'] &&
-          window['owpbjs'].getConfig('userSync') &&
-          window['owpbjs'].getConfig('userSync').hasOwnProperty('enableOverride') &&
-          window['owpbjs'].getConfig('userSync')['enableOverride']) {
-          owpbjs.triggerUserSyncs();
-        }
       }
 
       const validBidRequests = bidderRequest.bids.filter(filterAndWarn);
