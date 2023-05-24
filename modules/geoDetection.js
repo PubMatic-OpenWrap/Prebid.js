b181bb5ef30deaef432c6869a34a7fbf2b5358d3
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-05-24 18:23:41 +0530

    Removed option to get URL and path from publisher

--- modules/geoDetection.js
+++ modules/geoDetection.js
@@ -1,23 +1,16 @@
 import { ajaxBuilder } from '../src/ajax.js';
-import { deepAccess } from '../src/utils.js';
 
-var defaultGDURL = 'https://www.ebay.com/defaultLocation.json'; //TODO update this
-var defaultPath = 'location.region'; //TODO update this
 const TIMEOUT = 500;
 
 /*
     GeoDetection module is to be used to get the region information.
     This needs to be called with the URL of API and path of region (e.g. location.data.region)
 */
-$$PREBID_GLOBAL$$.detectLocation = function(URL = defaultGDURL, regionPath = defaultPath, callback) {
+$$PREBID_GLOBAL$$.detectLocation = function(URL, callback) {
     getRegion = function(loc) {
         try {
             let location = JSON.parse(loc);
-            if(location.error) {
-                callback(location);
-            } else {
-                callback({region: deepAccess(location, regionPath)});
-            }
+            callback(location);
         } catch(e) {
             console.log("Location data is expected to be an object");
             callback({error: e});
