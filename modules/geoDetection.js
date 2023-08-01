4a2b4a113e43fbc6a586225c1c9060ee23410a0e
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-08-01 15:30:24 +0530

    Fixed linting errors

--- modules/geoDetection.js
+++ modules/geoDetection.js
@@ -1,3 +1,4 @@
+import { logInfo, logError } from './../src/utils.js';
 import { ajaxBuilder } from '../src/ajax.js';
 import { getStorageManager } from '../src/storageManager.js';
 
@@ -7,56 +8,56 @@ const TIMEOUT = 500;
     GeoDetection module is to be used to get the region information.
     This needs to be called with the URL of API and path of region (e.g. location.data.region)
 */
-$$PREBID_GLOBAL$$.detectLocation = function(URL, callback) {
-    getRegion = function(loc) {
-        try {
-            let location = JSON.parse(loc);
-            callback(location);
-        } catch(e) {
-            console.log("Location data is expected to be an object");
-            callback({error: e});
-        }
-    }
-
+$$PREBID_GLOBAL$$.detectLocation = function(URL, passBack) {
+  const getRegion = function(loc) {
     try {
-        ajaxBuilder(TIMEOUT)(
-            URL,
-            { success: getRegion, error: function(e) {callback({error: e})} },
-            null,
-            { contentType: 'application/x-www-form-urlencoded', method: 'GET' }
-        );
-    } catch(e) {
-        callback({error: e});
+      let location = JSON.parse(loc);
+      passBack(location);
+    } catch (e) {
+      logInfo('Location data is expected to be an object');
+      passBack({error: e});
     }
+  }
+
+  try {
+    ajaxBuilder(TIMEOUT)(
+      URL,
+      { success: getRegion, error: function(e) { passBack({error: e}) } },
+      null,
+      { contentType: 'application/x-www-form-urlencoded', method: 'GET' }
+    );
+  } catch (e) {
+    passBack({error: e});
+  }
 }
 
 var BIDDER_CODE = 'pubmatic';
 var storage = getStorageManager({bidderCode: BIDDER_CODE});
 
 $$PREBID_GLOBAL$$.getDataFromLocalStorage = function(key, expiry) {
-	try {
-		var storedObject = storage.getDataFromLocalStorage(key);
-		if(storedObject) {
-			var createdDate = JSON.parse(storedObject).createdDate;
-			let currentDate = new Date().valueOf();
-			const diff = Math.abs(currentDate - createdDate);
-			if (diff > expiry) {
-			  storage.removeDataFromLocalStorage(key);
-			  return undefined;
-			}
-			return storedObject;	
-		}
-		return undefined;
-	} catch(e) {
-		return undefined;
-	}
+  try {
+    var storedObject = storage.getDataFromLocalStorage(key);
+    if (storedObject) {
+      var createdDate = JSON.parse(storedObject).createdDate;
+      let currentDate = new Date().valueOf();
+      const diff = Math.abs(currentDate - createdDate);
+      if (diff > expiry) {
+        storage.removeDataFromLocalStorage(key);
+        return undefined;
+      }
+      return storedObject;
+    }
+    return undefined;
+  } catch (e) {
+    return undefined;
+  }
 }
 
 $$PREBID_GLOBAL$$.setAndStringifyToLocalStorage = function(key, object) {
-	try {
-	  object.createdDate = new Date().valueOf();
-	  storage.setDataInLocalStorage(key, JSON.stringify(object));
-	} catch (e) {
-		refThis.logError("Error in setting localstorage ", e);
-	}
+  try {
+    object.createdDate = new Date().valueOf();
+    storage.setDataInLocalStorage(key, JSON.stringify(object));
+  } catch (e) {
+    logError('Error in setting localstorage ', e);
+  }
 }
