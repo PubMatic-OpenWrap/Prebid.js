2edb1554b1e2481e9d652dbaa7deced75f629934
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2024-05-10 14:53:32 +0530

    Ow as a module: added gulp task for setting Targetting and Native keys

--- gulpfile.js
+++ gulpfile.js
@@ -507,7 +507,38 @@ gulp.task('update-namespace', async function () {
   }
 });
 
-gulp.task('ow-tasks', gulp.series('append-footer','update-namespace'));
+gulp.task('bundle-pwt-keys', function() {
+  var usePBJSKeysEnabled = argv.usePBJSKeys || false;
+  var pubAnalyticsAdapterEnabled = argv.pubAnalyticsAdapter || false;
+  if(!usePBJSKeysEnabled && pubAnalyticsAdapterEnabled){
+      console.log("We need to use PWT keys, so changing targeting keys in PrebidJS config");
+      return gulp.src('build/*/'+ getBundleName(), { "allowEmpty": true })
+          .pipe(replace(/"%%TG_KEYS%%"/g,'{"STATUS":"pwtbst","BIDDER":"pwtpid","AD_ID":"pwtsid","PRICE_BUCKET":"pwtecp","SIZE":"pwtsz","DEAL":"pwtdeal","DEAL_ID":"pwtdid","SOURCE":"","FORMAT":"pwtplt","UUID":"pwtuuid","CACHE_ID":"pwtcid","CACHE_HOST":"pwtcurl","ADOMAIN":"pwtadomain"}'))
+          .pipe(gulp.dest('build/'));        
+  } else {
+      console.log("We need to use Prebid keys, so changing targeting keys in PrebidJS config");
+      return gulp.src('build/*/'+ getBundleName(), { "allowEmpty": true })
+          .pipe(replace(/"%%TG_KEYS%%"/g,'{"BIDDER":"hb_bidder","AD_ID":"hb_adid","PRICE_BUCKET":"hb_pb","SIZE":"hb_size","DEAL":"hb_deal","SOURCE":"hb_source","FORMAT":"hb_format","UUID":"hb_uuid","CACHE_ID":"hb_cache_id","CACHE_HOST":"hb_cache_host","ADOMAIN":"hb_adomain","ACAT":"hb_acat","CRID":"hb_crid","DSP":"hb_dsp"}'))
+          .pipe(gulp.dest('build/'));
+  }
+});
+
+gulp.task('bundle-native-keys', function() {
+  var usePBJSKeysEnabled = argv.usePBJSKeys || false;
+  if(usePBJSKeysEnabled) {
+      console.log("We need to use Prebid keys for Native, so changing targeting keys in PrebidJS config");
+      return gulp.src('build/*/'+ getBundleName(), { "allowEmpty": true })
+      .pipe(replace(/"%%TG_NATIVE_KEYS%%"/g,'{"title":"hb_native_title","body":"hb_native_body","body2":"hb_native_body2","privacyLink":"hb_native_privacy","privacyIcon":"hb_native_privicon","sponsoredBy":"hb_native_brand","image":"hb_native_image","icon":"hb_native_icon","clickUrl":"hb_native_linkurl","displayUrl":"hb_native_displayurl","cta":"hb_native_cta","rating":"hb_native_rating","address":"hb_native_address","downloads":"hb_native_downloads","likes":"hb_native_likes","phone":"hb_native_phone","price":"hb_native_price","salePrice":"hb_native_saleprice","rendererUrl":"hb_renderer_url","adTemplate":"hb_adTemplate"}'))
+      .pipe(gulp.dest('build/'));
+  } else {
+      console.log("We need to use PWT keys for Native, so changing targeting keys in PrebidJS config");
+      return gulp.src('build/*/'+ getBundleName(), { "allowEmpty": true })
+      .pipe(replace(/"%%TG_NATIVE_KEYS%%"/g,'{"title":"pwt_native_title","body":"pwt_native_body","body2":"pwt_native_body2","privacyLink":"pwt_native_privacy","sponsoredBy":"pwt_native_brand","image":"pwt_native_image","icon":"pwt_native_icon","clickUrl":"pwt_native_linkurl","displayUrl":"pwt_native_displayurl","cta":"pwt_native_cta","rating":"pwt_native_rating","address":"pwt_native_address","downloads":"pwt_native_downloads","likes":"pwt_native_likes","phone":"pwt_native_phone","price":"pwt_native_price","salePrice":"pwt_native_saleprice"}'))
+      .pipe(gulp.dest('build/'));
+  }
+});
+
+gulp.task('ow-tasks', gulp.series('append-footer','update-namespace', 'bundle-pwt-keys', 'bundle-native-keys'));
 
 // END: OW Custom tasks
 
--- modules/openWrap/conf.js
+++ modules/openWrap/conf.js
@@ -7,6 +7,9 @@ export let test_pwt = {};
 export let adapters = {};
 export let identityPartners = {};
 export let slotConfig = {};
+export let alias = {};
+export let test_adapters = {};
+export let test_identityPartners = {};
 
 function setOWConfig(owConfig) {
   if (!owConfig || typeof owConfig !== 'object') {
@@ -19,8 +22,10 @@ function setOWConfig(owConfig) {
   adapters = owConfig.adapters;
   identityPartners = owConfig.identityPartners;
   slotConfig = owConfig.slotConfig;
+  alias = owConfig.alias;
+  test_adapters = owConfig.test_adapters;
+  test_identityPartners = owConfig.test_identityPartners;
+
 };
 
 config.getConfig('openWrap', config => setOWConfig(config.openWrap));
-
-//export { pwt, testConfigDetails, test_pwt, adapters, identityPartners, slotConfig };
