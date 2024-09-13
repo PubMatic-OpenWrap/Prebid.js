15c3f7ca220f8603d516753ff0647f640b82d502
Nitin Nimbalkar <nitin.nimbalkar@pubmatic.com>
2024-09-13 13:51:47 +0530

    UID2 email hash issue

--- modules/userId/index.js
+++ modules/userId/index.js
@@ -842,6 +842,26 @@ export function getRawPDString(emailHashes, userID) {
   return btoa(pdString);
 };
 
+function hexToBytes(hex) {
+  return Uint8Array.from(
+    hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
+  );
+}
+
+// Module to convert byte array to Base64
+function bytesToBase64(bytes) {
+  const binaryString = String.fromCharCode(...bytes);
+  return btoa(binaryString);
+}
+
+export function getHexToBase64(hex) {
+  if (!hex || typeof hex !== 'string' || hex.trim() === '') {
+    logError(`Invalid hex input: hex string is undefined, null, or empty.`);
+    return undefined;
+  }
+  return bytesToBase64(hexToBytes(hex)); // Convert byte array to Base64
+}
+
 export function updateModuleParams(moduleToUpdate) {
   let params = MODULE_PARAM_TO_UPDATE_FOR_SSO[moduleToUpdate.name];
   if (!params) return;
@@ -850,7 +870,17 @@ export function updateModuleParams(moduleToUpdate) {
   let enableSSO = (window.IHPWT && window.IHPWT.ssoEnabled) || (window.PWT && window.PWT.ssoEnabled) || false;
   let emailHashes = enableSSO && userIdentity.emailHash ? userIdentity.emailHash : userIdentity.pubProvidedEmailHash ? userIdentity.pubProvidedEmailHash : undefined;
   params.forEach(function(param) {
-    moduleToUpdate.params[param.key] = (moduleToUpdate.name === 'id5Id' ? getRawPDString(emailHashes, userIdentity.userID) : emailHashes ? emailHashes[param.hashType] : undefined);
+    switch (moduleToUpdate.name) {
+      case 'id5Id':
+        moduleToUpdate.params[param.key] = getRawPDString(emailHashes, userIdentity.userID);
+        break;
+      case 'uid2':
+        moduleToUpdate.params[param.key] = getHexToBase64(emailHashes ? emailHashes[param.hashType] : undefined);
+        break;
+      default:
+        moduleToUpdate.params[param.key] = emailHashes ? emailHashes[param.hashType] : undefined;
+        break;
+    }
   });
 }
 
