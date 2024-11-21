9d15eb38abbedc2304b69e84616c9531f6289648
pm-nitin-shirsat <107102698+pm-nitin-shirsat@users.noreply.github.com>
2024-11-21 20:58:50 +0530

    Cheery pick IBV changes from vanilla prebid js

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -879,6 +879,18 @@ function _handleEids(payload, validBidRequests) {
   }
 }
 
+// Setting IBV & meta.mediaType field into the bid response
+export function setIBVField(bid, newBid) {
+  if (bid?.ext?.ibv) {
+    newBid.ext = newBid.ext || {};
+    newBid.ext['ibv'] = bid.ext.ibv;
+
+    // Overriding the mediaType field in meta with the `video` value if bid.ext.ibv is present
+    newBid.meta = newBid.meta || {};
+    newBid.meta.mediaType = VIDEO;
+  }
+}
+
 function _checkMediaType(bid, newBid) {
   // Create a regex here to check the strings
   if (bid.ext && bid.ext['bidtype'] != undefined) {
@@ -1028,7 +1040,7 @@ function isNonEmptyArray(test) {
  * @param {*} bid : bids
  */
 export function prepareMetaObject(br, bid, seat) {
-  br.meta = {};
+  br.meta = br.meta || {};
 
   if (bid.ext && bid.ext.dspid) {
     br.meta.networkId = bid.ext.dspid;
@@ -1066,6 +1078,11 @@ export function prepareMetaObject(br, bid, seat) {
 
   if (bid.ext && bid.ext.dsa && Object.keys(bid.ext.dsa).length) {
     br.meta.dsa = bid.ext.dsa;
+  }
+
+  // Initializing meta.mediaType field to the actual bidType returned by the bidder
+  if (br.mediaType) {
+    br.meta.mediaType = br.mediaType;
   }
 }
 
@@ -1486,7 +1503,7 @@ export const spec = {
                 newBid['dealChannel'] = dealChannelValues[bid.ext.deal_channel] || null;
               }
               prepareMetaObject(newBid, bid, seatbidder.seat);
-
+			  setIBVField(bid, newBid);
               // START of Experimental change
               if (response.body.ext) {
                 newBid['ext'] = response.body.ext;
