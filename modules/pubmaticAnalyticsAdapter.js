486fba824b0c95022f951ec060a5f4e8b2b368db
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2025-02-21 13:28:59 +0530

    Adding ds, ffs & pos changes

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -734,6 +734,10 @@ function executeBidWonLoggerCall(auctionId, adUnitId, isIma) {
   pixelURL += '&rf=' + enc(origAdUnit?.pubmaticAutoRefresh?.isRefreshed ? 1 : 0);
   pixelURL += '&origbidid=' + enc(winningBid?.bidResponse?.partnerImpId || winningBidId);
   pixelURL += '&di=' + enc(winningBid?.bidResponse?.dealId || OPEN_AUCTION_DEAL_ID);
+  const ds = winningBid.bidResponse?.meta ? getMetadata(winningBid.bidResponse.meta).ds : undefined;
+  if (ds) {
+    pixelURL += '&ds=' + enc(ds);
+  }
   pg && (pixelURL += '&pb=' + enc(pg));
 
   pixelURL += '&plt=' + enc(getDevicePlatform());
@@ -744,15 +748,17 @@ function executeBidWonLoggerCall(auctionId, adUnitId, isIma) {
   pixelURL += '&orig=' + enc(getDomainFromUrl(referrer));
   pixelURL += '&ss=' + enc(isS2SBidder(winningBid.bidder));
   (fskp != undefined) && (pixelURL += '&fskp=' + enc(fskp));
-  if (floorData && floorFetchStatus) {
+  if (floorData) {
     const floorRootValues = getFloorsCommonField(floorData.floorRequestData);
-    const { fsrc, fp, mv } = floorRootValues;
-    const params = { fsrc, fp, fmv: mv };
-    Object.entries(params).forEach(([key, value]) => {
-      if (value !== undefined) {
-        pixelURL += `&${key}=${enc(value)}`;
-      }
-    });
+    if (floorRootValues) {
+      const { ffs, fsrc, fp, mv } = floorRootValues;
+      const params = { ffs, fsrc, fp, fmv: mv };
+      Object.entries(params).forEach(([key, value]) => {
+        if (value !== undefined) {
+          pixelURL += `&${key}=${enc(value)}`;
+        }
+      });
+    }
     const floorType = getFloorType(floorData.floorResponseData);
     if (floorType !== undefined) {
       pixelURL += '&ft=' + enc(floorType);
