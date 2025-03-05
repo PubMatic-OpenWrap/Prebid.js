05d92ca33bbcc2cbffc1bed5f74f7d1d2b12d332
Tanishka Vishwakarma <tanishkavishwakarma@L1837.local>
2025-03-05 17:45:33 +0530

    UOE-11862-fixing changes for bidderCode

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -480,7 +480,7 @@ const eventHandlers = {
     bid.partnerTimeToRespond = latency > (auctionTime + 150) ? (auctionTime + 150) : latency;
     bid.clientLatencyTimeMs = Date.now() - cache.auctions[args.auctionId].timestamp;
     bid.bidResponse = parseBidResponse(args);
-    bid.bidderCode =  args.bidderCode;
+    bid.bidderCode =  args.bidderCode || bid.bidderCode;;
     bid.adapterName = getAdapterNameForAlias(args.adapterCode || bid.bidderCode);
   },
 
@@ -495,7 +495,9 @@ const eventHandlers = {
   },
 
   bidderDone: (args) => {
-    cache.auctions[args.auctionId].bidderDonePendingCount--;
+    if(cache.auctions[args.auctionId]?.bidderDonePendingCount){
+      cache.auctions[args.auctionId].bidderDonePendingCount--;
+    }
     args.bids.forEach(bid => {
       let cachedBid = cache.auctions[bid.auctionId].adUnitCodes[bid.adUnitCode].bids[bid.bidId || bid.originalRequestId || bid.requestId];
       if (typeof bid.serverResponseTimeMs !== 'undefined') {
