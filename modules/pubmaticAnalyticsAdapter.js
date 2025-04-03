6d199110cfd2ee6dbf2ec32dbca9286c914ff828
Tanishka Vishwakarma <tanishkavishwakarma@L1837.local>
2025-04-03 11:04:00 +0530

    UOE-11862:fixing issue for hook call

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -1,5 +1,5 @@
 import { _each, isArray, logError, logWarn, pick, isFn } from '../src/utils.js';
-import {adapter,setDebounceDelay} from '../libraries/analyticsAdapter/AnalyticsAdapter.js';
+import { default as adapter, setDebounceDelay } from '../libraries/analyticsAdapter/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
 import { BID_STATUS, STATUS, REJECTION_REASON } from '../src/constants.js';
 import { ajax } from '../src/ajax.js';
