566a72c17a1c41a36b24297652bb796f9f8fd967
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2025-04-04 12:46:10 +0530

    Test cases for conf.js

--- modules/openWrap/conf.js
+++ modules/openWrap/conf.js
@@ -11,20 +11,20 @@ export let alias = {};
 export let test_adapters = {};
 export let test_identityPartners = {};
 
-function setOWConfig(owConfig) {
+export function setOWConfig(owConfig) {
   if (!owConfig || typeof owConfig !== 'object') {
     logError('OpenWrap config not defined...');
     return;
   }
-  pwt = owConfig.pwt;
-  testConfigDetails = owConfig.testConfigDetails;
-  test_pwt = owConfig.test_pwt;
-  adapters = owConfig.adapters;
-  identityPartners = owConfig.identityPartners;
-  slotConfig = owConfig.slotConfig;
-  alias = owConfig.alias;
-  test_adapters = owConfig.test_adapters;
-  test_identityPartners = owConfig.test_identityPartners;
+  pwt = owConfig.pwt || {};
+  testConfigDetails = owConfig.testConfigDetails || {};
+  test_pwt = owConfig.test_pwt || {};
+  adapters = owConfig.adapters || {};
+  identityPartners = owConfig.identityPartners || {};
+  slotConfig = owConfig.slotConfig || {};
+  alias = owConfig.alias || {};
+  test_adapters = owConfig.test_adapters || {};
+  test_identityPartners = owConfig.test_identityPartners || {};
 };
 
 config.getConfig('openWrap', config => setOWConfig(config.openWrap));
