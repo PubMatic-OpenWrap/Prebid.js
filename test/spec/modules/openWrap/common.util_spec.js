1a1cf3139b630d2ae85bbc855ea2aa1d8e838361
Komal Kumari <komalkumari@Komals-MacBook-Pro-L1834.local>
2025-04-08 15:40:23 +0530

    Code optimization

--- test/spec/modules/openWrap/common.util_spec.js
+++ test/spec/modules/openWrap/common.util_spec.js
@@ -1,19 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import * as UTIL from '../../../../modules/openWrap/common.util.js';
-import * as CONFIG from '../../../../modules/openWrap/config.js';
 
 describe('COMMON UTIL', () => {
     describe('#getGeoInfo', () => {
         let pbNameSpace;
-        
+
         beforeEach((done) => {
             // Mock the namespace based on identity only flag
             pbNameSpace = "owpbjs"; // or "ihowpbjs" based on identity flag
             window[pbNameSpace] = {
-                getDataFromLocalStorage: () => {},
-                detectLocation: () => {},
-                setAndStringifyToLocalStorage: () => {}
+                getDataFromLocalStorage: () => { },
+                detectLocation: () => { },
+                setAndStringifyToLocalStorage: () => { }
             };
             done();
         });
@@ -28,7 +27,7 @@ describe('COMMON UTIL', () => {
                 cc: "US"
             };
             const getDataStub = sinon.stub(window[pbNameSpace], 'getDataFromLocalStorage').returns(JSON.stringify(mockGeoData));
-            
+
             UTIL.getGeoInfo({ LOCALSTORAGE: 'localStorage' }, (source) => {
                 expect(window.PWT.CC).to.deep.equal(mockGeoData);
                 expect(source).to.equal('localStorage');
@@ -42,15 +41,15 @@ describe('COMMON UTIL', () => {
             const detectLocationStub = sinon.stub(window[pbNameSpace], 'detectLocation').callsFake((url, callback) => {
                 callback({ cc: "US" }, true);
             });
-            
-            UTIL.getGeoInfo({ 
-                LOCALSTORAGE: 'localStorage', 
-                GEO_SERVICE: 'geoService' 
+
+            UTIL.getGeoInfo({
+                LOCALSTORAGE: 'localStorage',
+                GEO_SERVICE: 'geoService'
             }, (source, loc) => {
                 expect(window.PWT.CC).to.deep.equal({ cc: "US" });
                 expect(source).to.equal('geoService');
                 expect(loc).to.deep.equal({ cc: "US" });
-                
+
                 getDataStub.restore();
                 detectLocationStub.restore();
                 done();
@@ -60,38 +59,38 @@ describe('COMMON UTIL', () => {
 
     describe('#shouldThrottle', () => {
         let randomStub, floorStub;
-    
+
         beforeEach((done) => {
             randomStub = sinon.stub(Math, 'random');
             floorStub = sinon.stub(Math, 'floor');
             done();
         });
-    
+
         afterEach((done) => {
             randomStub.restore();
             floorStub.restore();
             done();
         });
-    
+
         it('is a function', (done) => {
             expect(UTIL.shouldThrottle).to.be.a('function');
             done();
         });
-    
+
         it('should return true when random value is greater than throttle rate', (done) => {
             randomStub.returns(0.9);
             floorStub.returns(90);
             expect(UTIL.shouldThrottle(80)).to.be.true;
             done();
         });
-    
+
         it('should return false when random value is less than throttle rate', (done) => {
             randomStub.returns(0.5);
             floorStub.returns(50);
             expect(UTIL.shouldThrottle(80)).to.be.false;
             done();
         });
-    
+
         it('should use default maxRandomValue when not provided', (done) => {
             randomStub.returns(0.5);
             floorStub.returns(50);
@@ -100,7 +99,7 @@ describe('COMMON UTIL', () => {
             expect(floorStub.calledWith(50)).to.be.true;
             done();
         });
-    
+
         it('should use provided maxRandomValue', (done) => {
             randomStub.returns(0.5);
             floorStub.returns(25);
