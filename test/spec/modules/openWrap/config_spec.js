d0fb0b9f3162dea64dae7960e5e082de877f81b1
Komal Kumari <komalkumari@Komals-MacBook-Pro-L1834.local>
2025-04-24 18:01:00 +0530

    Updated test cases for openwrap config.js

--- test/spec/modules/openWrap/config_spec.js
+++ test/spec/modules/openWrap/config_spec.js
@@ -938,6 +938,24 @@ describe('OpenWrap Core Module: config.js', function () {
   });
 
   describe('Partner Configuration Update', function () {
+    let logStub, isOwnPropertyStub, isObjectStub, getMergedConfigStub;
+    before(function () {
+      logStub = sinon.stub(util, 'log');
+      isOwnPropertyStub = sinon.stub(util, 'isOwnProperty').callsFake((obj, key) => Object.prototype.hasOwnProperty.call(obj, key));
+      isObjectStub = sinon.stub(util, 'isObject').callsFake(obj => typeof obj === 'object' && obj !== null && !Array.isArray(obj));
+      getMergedConfigStub = sinon.stub(configModule, 'getMergedConfig').callsFake((to, from) => ({ ...to, ...from }));
+      // Ensure window.PWT exists for the test
+      if (typeof window === 'undefined') global.window = {};
+      window.PWT = window.PWT || {};
+    });
+    after(function () {
+      logStub.restore();
+      isOwnPropertyStub.restore();
+      isObjectStub.restore();
+      getMergedConfigStub.restore();
+      // Cleanup window.PWT only
+      if (window.PWT) delete window.PWT;
+    });
 
     it('updatePartnerConfig should handle empty test configuration', function () {
       const testConfig = null;
@@ -955,6 +973,21 @@ describe('OpenWrap Core Module: config.js', function () {
       const result = configModule.updatePartnerConfig(testConfig, controlConfig);
       
       expect(result).to.equal(controlConfig);
+    });
+
+    it('updatePartnerConfig should update testConfig with merged values and set testGroupId when both configs are present and valid', function () {
+      // Arrange
+      const testConfig = { adapter1: { key1: 'value1' } };
+      const controlConfig = { adapter1: { key1: 'value1' } };
+      // Act
+      const result = configModule.updatePartnerConfig(testConfig, controlConfig);
+      // Assert
+      expect(logStub.calledOnce).to.be.true;
+      expect(logStub.firstCall.args[0]).to.equal(CONSTANTS.MESSAGES.M31);
+      expect(JSON.parse(logStub.firstCall.args[1])).to.deep.equal(testConfig);
+      expect(result).to.equal(testConfig);
+      expect(result.adapter1).to.deep.equal({ key1: 'value1' });
+      expect(window.PWT.testGroupId).to.equal(1);
     });
   });
 
@@ -1022,6 +1055,247 @@ describe('OpenWrap Core Module: config.js', function () {
       });
       
       expect(configModule.getBidPassThroughStatus('testAdapter')).to.equal(0);
+    });
+  });
+
+  describe('getIdentityPartners', function () {
+    afterEach(function () {
+      delete conf.identityPartners;
+    });
+
+    it('should return the configured identity partners', function () {
+      const partners = { id5: {}, sharedId: {} };
+      conf.identityPartners = partners;
+      expect(configModule.getIdentityPartners()).to.deep.equal(partners);
+    });
+
+    it('should return undefined if identity partners are not set', function () {
+      delete conf.identityPartners;
+      expect(configModule.getIdentityPartners()).to.equal(undefined);
+    });
+  });
+
+  describe('getAdServer', function () {
+    afterEach(function () {
+      delete conf.adserver;
+    });
+
+    it('should return the configured ad server', function () {
+      conf.adserver = 'dfp';
+      expect(configModule.getAdServer()).to.equal('dfp');
+    });
+
+    it('should return undefined if ad server is not set', function () {
+      delete conf.adserver;
+      expect(configModule.getAdServer()).to.equal(undefined);
+    });
+  });
+
+  describe('getPriceGranularity', function () {
+    it('should return the configured price granularity when set to a standard value', function () {
+      conf.setOWConfig({
+        [CONSTANTS.CONFIG.COMMON]: {
+          [CONSTANTS.COMMON.PRICE_GRANULARITY]: 'medium'
+        }
+      });
+      expect(configModule.getPriceGranularity()).to.equal('medium');
+    });
+
+    it('should return null when price granularity is not set', function () {
+      conf.setOWConfig({ [CONSTANTS.CONFIG.COMMON]: {} });
+      expect(configModule.getPriceGranularity()).to.equal(null);
+    });
+
+    it('should return transformed buckets when price granularity is custom and buckets are defined', function () {
+      const customBuckets = {
+        ranges: [
+          { max: 5, increment: 0.5 },
+          { max: 10, increment: 1 }
+        ],
+        precision: 2
+      };
+      conf.setOWConfig({
+        [CONSTANTS.CONFIG.COMMON]: {
+          [CONSTANTS.COMMON.PRICE_GRANULARITY]: CONSTANTS.COMMON.PRICE_GRANULARITY_CUSTOM,
+          [CONSTANTS.COMMON.PRICE_GRANULARITY_BUCKETS]: customBuckets
+        }
+      });
+      const result = configModule.getPriceGranularity();
+      expect(result).to.have.property('buckets');
+      expect(result.buckets).to.deep.equal(customBuckets.ranges);
+      expect(result.precision).to.equal(2);
+    });
+
+    it('should log warning and return null when price granularity is custom but buckets are missing', function () {
+      const logWarningStub = sinon.stub(util, 'logWarning');
+      conf.setOWConfig({
+        [CONSTANTS.CONFIG.COMMON]: {
+          [CONSTANTS.COMMON.PRICE_GRANULARITY]: CONSTANTS.COMMON.PRICE_GRANULARITY_CUSTOM
+        }
+      });
+      const result = configModule.getPriceGranularity();
+      expect(result).to.equal(null);
+      expect(logWarningStub.calledOnce).to.be.true;
+      logWarningStub.restore();
+    });
+  });
+
+  describe('getNativeConfiguration', function () {
+    afterEach(function () {
+      delete conf.nativeConfig;
+    });
+
+    it('should return the configured native media type config', function () {
+      const nativeConfig = { image: { required: true }, title: { len: 80 } };
+      conf.nativeConfig = nativeConfig;
+      expect(configModule.getNativeConfiguration()).to.deep.equal(nativeConfig);
+    });
+
+    it('should return undefined if native media type config is not set', function () {
+      delete conf.nativeConfig;
+      expect(configModule.getNativeConfiguration()).to.equal(undefined);
+    });
+  });
+
+  describe('enableBidpoolingIfApplicable', function () {
+    let originalPwt;
+    beforeEach(function () {
+      // Save and reset conf.pwt before each test
+      originalPwt = JSON.parse(JSON.stringify(conf.pwt));
+      if (!conf.pwt) conf.pwt = {};
+    });
+    afterEach(function () {
+      // Restore conf.pwt after each test
+      conf.pwt = JSON.parse(JSON.stringify(originalPwt));
+    });
+
+    it('should enable bid pooling if testType matches BID_POOLING', function () {
+      const testGroupDetails = { testType: CONSTANTS.COMMON.BID_POOLING };
+      delete conf.pwt[CONSTANTS.COMMON.BID_POOLING_ENABLED];
+      configModule.enableBidpoolingIfApplicable(testGroupDetails);
+      expect(conf.pwt[CONSTANTS.COMMON.BID_POOLING_ENABLED])
+        .to.equal(CONSTANTS.COMMON.ENABLED_BID_POOLING);
+    });
+
+    it('should not enable bid pooling if testType does not match BID_POOLING', function () {
+      const testGroupDetails = { testType: 'not-bid-pooling' };
+      delete conf.pwt[CONSTANTS.COMMON.BID_POOLING_ENABLED];
+      configModule.enableBidpoolingIfApplicable(testGroupDetails);
+      expect(conf.pwt[CONSTANTS.COMMON.BID_POOLING_ENABLED]).to.be.undefined;
+    });
+
+    it('should not enable bid pooling if testGroupDetails is missing testType', function () {
+      const testGroupDetails = {};
+      configModule.enableBidpoolingIfApplicable(testGroupDetails);
+      expect(conf.pwt[CONSTANTS.COMMON.BID_POOLING_ENABLED]).to.be.undefined;
+    });
+
+    it('should not mutate unrelated config properties', function () {
+      conf.pwt.otherProperty = 'shouldRemain';
+      const testGroupDetails = { testType: 'not-bid-pooling' };
+      configModule.enableBidpoolingIfApplicable(testGroupDetails);
+      expect(conf.pwt.otherProperty).to.equal('shouldRemain');
+    });
+  });
+
+  describe('updatePWTConfig', function () {
+    let sandbox;
+    let originalConfig;
+    beforeEach(function () {
+      sandbox = sinon.createSandbox();
+      // Use deep clone to preserve the original state
+      originalConfig = JSON.parse(JSON.stringify(conf.pwt));
+      if (!conf.pwt) conf.pwt = {};
+      // Clear TEST_PWT and pwt config
+      conf[CONSTANTS.COMMON.TEST_PWT] = undefined;
+      conf.pwt = { foo: 'bar', bar: 'baz' };
+    });
+    afterEach(function () {
+      sandbox.restore();
+      conf.pwt = JSON.parse(JSON.stringify(originalConfig));
+      conf[CONSTANTS.COMMON.TEST_PWT] = undefined;
+    });
+
+    it('should update config.pwt keys with testConfig values and log', function () {
+      // Arrange
+      const testConfig = { foo: 'updated', bar: 'baz2', notPresent: 'no' };
+      conf[CONSTANTS.COMMON.TEST_PWT] = testConfig;
+      const logStub = sandbox.stub(util, 'log');
+      // Act
+      configModule.updatePWTConfig();
+      // Assert
+      expect(conf.pwt.foo).to.equal('updated');
+      expect(conf.pwt.bar).to.equal('baz2');
+      // Should not add new keys not present in config.pwt
+      expect(conf.pwt.notPresent).to.be.undefined;
+      expect(logStub.calledOnce).to.be.true;
+      expect(logStub.firstCall.args[0]).to.equal(CONSTANTS.MESSAGES.M30);
+      expect(JSON.parse(logStub.firstCall.args[1])).to.deep.equal(testConfig);
+    });
+
+    it('should not update config if testConfig is empty', function () {
+      conf[CONSTANTS.COMMON.TEST_PWT] = {};
+      const logStub = sandbox.stub(util, 'log');
+      configModule.updatePWTConfig();
+      expect(conf.pwt).to.deep.equal({ foo: 'bar', bar: 'baz' });
+      expect(logStub.called).to.be.false;
+    });
+
+    it('should not update config if testConfig is undefined', function () {
+      conf[CONSTANTS.COMMON.TEST_PWT] = undefined;
+      const logStub = sandbox.stub(util, 'log');
+      configModule.updatePWTConfig();
+      expect(conf.pwt).to.deep.equal({ foo: 'bar', bar: 'baz' });
+      expect(logStub.called).to.be.false;
+    });
+
+    it('should skip keys not present in config.pwt', function () {
+      conf[CONSTANTS.COMMON.TEST_PWT] = { notPresent: 'no' };
+      configModule.updatePWTConfig();
+      expect(conf.pwt.notPresent).to.be.undefined;
+    });
+  });
+
+  describe('initConfig', function () {
+    let sandbox;
+    beforeEach(function () {
+      sandbox = sinon.createSandbox();
+      // Stub all side-effect functions
+      sandbox.stub(configModule, 'updateABTestConfig');
+      sandbox.stub(configModule, 'addPrebidAdapter');
+      sandbox.stub(util, 'forEachOnObject').callsFake((obj, cb) => {
+        // Call cb for each key/value in obj
+        for (const key in obj) {
+          if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            cb(key, obj[key]);
+          }
+        }
+      });
+      // Set up adapters and constants for slot-level param propagation
+      conf.adapters = {
+        testAdapter: {
+          param1: 'value1',
+          param2: 'value2',
+          klm: {
+            slot1: {},
+            slot2: {}
+          }
+        }
+      };
+      // Add KEY_LOOKUP_MAP to constants
+      CONSTANTS.CONFIG.KEY_LOOKUP_MAP = 'klm';
+    });
+    afterEach(function () {
+      sandbox.restore();
+    });
+
+    it('should propagate adapter-level params to slot-level config', function () {
+      configModule.initConfig();
+      // Slot-level params should be set
+      expect(conf.adapters.testAdapter.klm.slot1.param1).to.equal('value1');
+      expect(conf.adapters.testAdapter.klm.slot1.param2).to.equal('value2');
+      expect(conf.adapters.testAdapter.klm.slot2.param1).to.equal('value1');
+      expect(conf.adapters.testAdapter.klm.slot2.param2).to.equal('value2');
     });
   });
 });
