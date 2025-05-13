0d4b18348bf21b4b51a7b0eb09e242fb7cb59a5c
Jason Quaccia <jason.quaccia@pubmatic.com>
2025-05-13 15:25:27 -0700

    gzip compression feature

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -748,7 +748,10 @@ export const spec = {
       method: 'POST',
       url: ENDPOINT,
       data: data,
-      bidderRequest: bidderRequest
+      bidderRequest: bidderRequest,
+      options: {
+        endpointCompression: true
+      }
     };
     return data?.imp?.length ? serverRequest : null;
   },
