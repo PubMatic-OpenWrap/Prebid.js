b611bebdc07346805b3860cae4c606de8c5c1fba
Jason Quaccia <jason.quaccia@pubmatic.com>
2025-05-16 15:31:19 -0700

    removed json validation check

--- src/utils.js
+++ src/utils.js
@@ -1325,14 +1325,8 @@ export const isGzipCompressionSupported = (function () {
 
 // Make sure to use isGzipCompressionSupported before calling this function
 export async function compressDataWithGZip(data) {
-  if (typeof data !== 'string') {
+  if (typeof data !== 'string') { // TextEncoder (below) expects a string
     data = JSON.stringify(data);
-  }
-
-  try {
-    JSON.parse(data);
-  } catch (e) {
-    throw new Error('Data cannot be stringified to valid JSON.');
   }
 
   const encoder = new TextEncoder();
