e569c9785cadcaa37c45dd398da7ab1d55c896d6
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2025-06-07 12:20:34 +0530

    Test cases for common util and common config

--- test/spec/modules/openWrap/common.config_spec.js
+++ test/spec/modules/openWrap/common.config_spec.js
@@ -4,67 +4,56 @@ import * as COMMON_CONFIG from '../../../../modules/openWrap/common.config.js';
 import { expect } from 'chai';
 
 describe('OpenWrap Core Module: common.config.js', function () {
-  describe('#getGdprActionTimeout', function () {
+  describe('#getConsentManagementEnabled', function () {
     it('is a function', function () {
-      expect(COMMON_CONFIG.getGdprActionTimeout).to.be.a('function');
+      expect(COMMON_CONFIG.getConsentManagementEnabled).to.be.a('function');
     });
 
-    it('should return 5000, as it is set to 5000 when getGdprActionTimeout is called', function () {
-      CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.GDPR_ACTION_TIMEOUT] = 5000;
-      expect(COMMON_CONFIG.getGdprActionTimeout()).to.equal(5000);
+    it('should return true when consentManagementEnabled is set to 1', function () {
+      CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_ENABLED] = "1";
+      expect(COMMON_CONFIG.getConsentManagementEnabled()).to.be.true;
     });
 
-    it('should return default value for gdpr action timeout which is 0, as it is NOT set', function () {
-      delete CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.GDPR_ACTION_TIMEOUT];
-      expect(COMMON_CONFIG.getGdprActionTimeout()).to.equal(0);
+    it('should return false when consentManagementEnabled is set to 0', function () {
+      CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_ENABLED] = "0";
+      expect(COMMON_CONFIG.getConsentManagementEnabled()).to.be.false;
+    });
+
+    it('should return false when consentManagementEnabled is not set', function () {
+      delete CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_ENABLED];
+      expect(COMMON_CONFIG.getConsentManagementEnabled()).to.be.false;
     });
   });
 
-  describe('setConsentConfig function', function () {
-    let prebidConfig = {};
-    const cmpApi = 'iab';
-    const timeout = 2000;
-
-    it('setConsentConfig a function', function () {
-      expect(COMMON_CONFIG.setConsentConfig).to.be.a('function');
+  describe('#getCmpApi', function () {
+    it('is a function', function () {
+      expect(COMMON_CONFIG.getCmpApi).to.be.a('function');
     });
 
-    it('should set consent management config when its not present', function () {
-      const expPrebidConfig = {
-        consentManagement: {
-          gpp: {
-            cmpApi,
-            timeout
-          }
-        }
-      };
-      const actPrebidConfig = COMMON_CONFIG.setConsentConfig(prebidConfig, 'gpp', cmpApi, timeout);
-      expect(actPrebidConfig).to.be.deep.equal(expPrebidConfig);
+    it('should return configured CMP API', function () {
+      CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_CMPAPI] = 'iab';
+      expect(COMMON_CONFIG.getCmpApi()).to.equal('iab');
     });
 
-    it('should set consent management config when its present', function () {
-      const prebidConfig = {
-        consentManagement: {
-          gdpr: {
-            cmpApi,
-            timeout
-          }
-        }
-      };
-      const expPrebidConfig = {
-        consentManagement: {
-          gdpr: {
-            cmpApi,
-            timeout
-          },
-          gpp: {
-            cmpApi,
-            timeout
-          }
-        }
-      };
-      const actPrebidConfig = COMMON_CONFIG.setConsentConfig(prebidConfig, 'gpp', cmpApi, timeout);
-      expect(actPrebidConfig).to.be.deep.equal(expPrebidConfig);
+    it('should return default when not configured', function () {
+      delete CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_CMPAPI];
+      expect(COMMON_CONFIG.getCmpApi()).to.equal('iab');
+    });
+  });
+
+  describe('#getTimeout', function () {
+    it('is a function', function () {
+      expect(COMMON_CONFIG.getTimeout).to.be.a('function');
+    });
+
+    it('should return configured timeout', function () {
+      CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_TIMEOUT] = 2000;
+      expect(COMMON_CONFIG.getTimeout(CONSTANTS.CONFIG.CONSENT_MANAGEMENT_TIMEOUT, 0)).to.equal(2000);
+    });
+
+    it('should return default when not configured', function () {
+      delete CONF[CONSTANTS.CONFIG.COMMON][CONSTANTS.CONFIG.CONSENT_MANAGEMENT_TIMEOUT];
+      expect(COMMON_CONFIG.getTimeout(CONSTANTS.CONFIG.CONSENT_MANAGEMENT_TIMEOUT, 0)).to.equal(0);
     });
   });
 });
--- test/spec/modules/openWrap/common.util_spec.js
+++ test/spec/modules/openWrap/common.util_spec.js
@@ -1,6 +1,6 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
 import * as UTIL from '../../../../modules/openWrap/common.util.js';
+import * as CONSTANTS from '../../../../modules/openWrap/constants.js';
+import * as conf from '../../../../modules/openWrap/conf.js';
 
 describe('OpenWrap Core Module: common.util.js', () => {
   describe('#getGeoInfo', () => {
@@ -108,6 +108,235 @@ describe('OpenWrap Core Module: common.util.js', () => {
       expect(randomStub.calledOnce).to.be.true;
       expect(floorStub.calledWith(25)).to.be.true;
       done();
+    });
+  });
+
+  describe('#isA', () => {
+    it('is a function', () => {
+      expect(UTIL.isA).to.be.a('function');
+    });
+
+    it('should return true for correct object type', () => {
+      expect(UTIL.isA([], 'Array')).to.be.true;
+      expect(UTIL.isA({}, 'Object')).to.be.true;
+      expect(UTIL.isA('string', 'String')).to.be.true;
+      expect(UTIL.isA(42, 'Number')).to.be.true;
+      expect(UTIL.isA(true, 'Boolean')).to.be.true;
+      expect(UTIL.isA(() => {}, 'Function')).to.be.true;
+      expect(UTIL.isA(new Date(), 'Date')).to.be.true;
+    });
+
+    it('should return false for incorrect object type', () => {
+      expect(UTIL.isA([], 'Object')).to.be.false;
+      expect(UTIL.isA({}, 'Array')).to.be.false;
+      expect(UTIL.isA('string', 'Number')).to.be.false;
+      expect(UTIL.isA(42, 'String')).to.be.false;
+    });
+  });
+
+  describe('#isFunction', () => {
+    it('is a function', () => {
+      expect(UTIL.isFunction).to.be.a('function');
+    });
+
+    it('should return true for functions', () => {
+      expect(UTIL.isFunction(() => {})).to.be.true;
+      expect(UTIL.isFunction(function() {})).to.be.true;
+      expect(UTIL.isFunction(UTIL.isFunction)).to.be.true;
+    });
+
+    it('should return false for non-functions', () => {
+      expect(UTIL.isFunction({})).to.be.false;
+      expect(UTIL.isFunction([])).to.be.false;
+      expect(UTIL.isFunction('string')).to.be.false;
+      expect(UTIL.isFunction(42)).to.be.false;
+      expect(UTIL.isFunction(null)).to.be.false;
+      expect(UTIL.isFunction(undefined)).to.be.false;
+    });
+  });
+
+  describe('#isNumber', () => {
+    it('is a function', () => {
+      expect(UTIL.isNumber).to.be.a('function');
+    });
+
+    it('should return true for numbers', () => {
+      expect(UTIL.isNumber(42)).to.be.true;
+      expect(UTIL.isNumber(0)).to.be.true;
+      expect(UTIL.isNumber(-1)).to.be.true;
+      expect(UTIL.isNumber(3.14)).to.be.true;
+      expect(UTIL.isNumber(Number.MAX_VALUE)).to.be.true;
+    });
+
+    it('should return false for non-numbers', () => {
+      expect(UTIL.isNumber('42')).to.be.false;
+      expect(UTIL.isNumber({})).to.be.false;
+      expect(UTIL.isNumber([])).to.be.false;
+      expect(UTIL.isNumber(null)).to.be.false;
+      expect(UTIL.isNumber(undefined)).to.be.false;
+    });
+  });
+
+  describe('#isObject', () => {
+    it('is a function', () => {
+      expect(UTIL.isObject).to.be.a('function');
+    });
+
+    it('should return true for objects', () => {
+      expect(UTIL.isObject({})).to.be.true;
+      expect(UTIL.isObject([])).to.be.true;
+      expect(UTIL.isObject(new Date())).to.be.true;
+    });
+
+    it('should return false for non-objects', () => {
+      expect(UTIL.isObject(null)).to.be.false;
+      expect(UTIL.isObject(undefined)).to.be.false;
+      expect(UTIL.isObject('string')).to.be.false;
+      expect(UTIL.isObject(42)).to.be.false;
+      expect(UTIL.isObject(true)).to.be.false;
+      expect(UTIL.isObject(() => {})).to.be.false;
+    });
+  });
+
+  describe('#isEmptyObject', () => {
+    it('is a function', () => {
+      expect(UTIL.isEmptyObject).to.be.a('function');
+    });
+
+    it('should return true for empty objects', () => {
+      expect(UTIL.isEmptyObject({})).to.be.true;
+    });
+
+    it('should return false for non-empty objects', () => {
+      expect(UTIL.isEmptyObject({ key: 'value' })).to.be.false;
+      expect(UTIL.isEmptyObject([1, 2, 3])).to.be.false;
+    });
+
+    it('should return false for non-objects', () => {
+      expect(UTIL.isEmptyObject(null)).to.be.false;
+      expect(UTIL.isEmptyObject(undefined)).to.be.false;
+      expect(UTIL.isEmptyObject('string')).to.be.false;
+      expect(UTIL.isEmptyObject(42)).to.be.false;
+    });
+  });
+
+  describe('#getGlobalPbObject', () => {
+    beforeEach(() => {
+      if (!window.PWT) {
+        window.PWT = {};
+      }
+      sinon.stub(CONSTANTS, 'CONFIG').value({
+        'COMMON': 'pwt',
+        'PB_GLOBAL_VAR_NAMESPACE': 'pb_namespace'
+      });
+      sinon.stub(CONSTANTS, 'COMMON').value({
+        'IDENTITY_ONLY': 'identity_only',
+        'IH_NAMESPACE': 'ihowpbjs',
+        'PREBID_NAMESPACE': 'owpbjs'
+      });
+    });
+
+    afterEach(() => {
+      delete window.owpbjs;
+      delete window.ihowpbjs;
+      delete window.customPbjs;      
+    });
+
+    it('is a function', () => {
+      expect(UTIL.getGlobalPbObject).to.be.a('function');
+    });
+
+    it('should return default Prebid object when no custom namespace is set', () => {
+      const result = UTIL.getGlobalPbObject();
+      expect(result).to.equal(window.owpbjs);
+      expect(window.owpbjs).to.be.an('object');
+    });
+
+    it('should return identity hub namespace when identity_only is set to 1', () => {
+      conf.pwt.identity_only = '1';
+      const result = UTIL.getGlobalPbObject();
+      expect(result).to.equal(window.ihowpbjs);
+      expect(window.ihowpbjs).to.be.an('object');
+    });
+
+    it('should return custom namespace when set in config', () => {
+      conf.pwt.pb_namespace = 'customPbjs';
+      const result = UTIL.getGlobalPbObject();
+      expect(result).to.equal(window.customPbjs);
+      expect(window.customPbjs).to.be.an('object');
+    });
+  });
+
+  describe('#getGlobalOwObject', () => {
+    beforeEach(() => {
+      sinon.stub(CONSTANTS, 'CONFIG').value({
+        'COMMON': 'pwt',
+        'OW_GLOBAL_VAR_NAMESPACE': 'ow_namespace'
+      });
+      sinon.stub(CONSTANTS, 'COMMON').value({
+        'IDENTITY_ONLY': 'identity_only',
+        'IH_OW_NAMESPACE': 'IHPWT',
+        'OPENWRAP_NAMESPACE': 'PWT'
+      });
+    });
+
+    afterEach(() => {
+      delete window.PWT;
+      delete window.IHPWT;
+      delete window.customOw;
+    });
+
+    it('is a function', () => {
+      expect(UTIL.getGlobalOwObject).to.be.a('function');
+    });
+
+    it('should return identity hub namespace when identity_only is set to 1', () => {
+      conf.pwt.identity_only = '1';
+      const result = UTIL.getGlobalOwObject();
+      expect(result).to.equal(window.IHPWT);
+      expect(window.IHPWT).to.be.an('object');
+    });
+
+    it('should return custom namespace when set in config', () => {
+      conf.pwt.ow_namespace = 'customOw';
+      const result = UTIL.getGlobalOwObject();
+      expect(result).to.equal(window.customOw);
+      expect(window.customOw).to.be.an('object');
+    });
+  });
+
+  describe('#getKeyByValue', () => {
+    it('is a function', () => {
+      expect(UTIL.getKeyByValue).to.be.a('function');
+    });
+
+    it('should return key for a given value in an object', () => {
+      const obj = { key1: 'value1', key2: 'value2', key3: 'value3' };
+      expect(UTIL.getKeyByValue(obj, 'value2')).to.equal('key2');
+    });
+
+    it('should return first key when multiple keys have the same value', () => {
+      const obj = { key1: 'value', key2: 'value', key3: 'different' };
+      const result = UTIL.getKeyByValue(obj, 'value');
+      expect(result === 'key1' || result === 'key2').to.be.true;
+    });
+
+    it('should return null when value is not found', () => {
+      const obj = { key1: 'value1', key2: 'value2' };
+      expect(UTIL.getKeyByValue(obj, 'nonexistent')).to.be.null;
+    });
+
+    it('should handle empty objects', () => {
+      expect(UTIL.getKeyByValue({}, 'value')).to.be.null;
+    });
+
+    it('should handle non-string values', () => {
+      const obj = { key1: 1, key2: true, key3: null, key4: undefined, key5: { nested: 'object' } };
+      expect(UTIL.getKeyByValue(obj, 1)).to.equal('key1');
+      expect(UTIL.getKeyByValue(obj, true)).to.equal('key2');
+      expect(UTIL.getKeyByValue(obj, null)).to.equal('key3');
+      expect(UTIL.getKeyByValue(obj, undefined)).to.equal('key4');
+      expect(UTIL.getKeyByValue(obj, obj.key5)).to.equal('key5');
     });
   });
 });
