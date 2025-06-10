8dff670fff5e795d00e70087fd998b084a81e61f
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2025-06-10 14:57:51 +0530

    Handle both Single qoute and Double qoute for the TG keys

--- gulpfile.js
+++ gulpfile.js
@@ -592,12 +592,12 @@ gulp.task('bundle-pwt-keys', function() {
   if (!usePBJSKeysEnabled) {
     // console.log("We need to use PWT keys, so changing targeting keys in PrebidJS config");
     return gulp.src('build/*/' + getBundleName(), { 'allowEmpty': true })
-      .pipe(replace(/'%%TG_KEYS%%'/g, "{STATUS:'pwtbst',BIDDER:'pwtpid',AD_ID:'pwtsid',PRICE_BUCKET:'pwtecp',SIZE:'pwtsz',DEAL:'pwtdeal',DEAL_ID:'pwtdid',SOURCE:'',FORMAT:'pwtplt',UUID:'pwtuuid',CACHE_ID:'pwtcid',CACHE_HOST:'pwtcurl',ADOMAIN:'pwtadomain'}"))
+      .pipe(replace(/['"]%%TG_KEYS%%['"]/g, "{STATUS:'pwtbst',BIDDER:'pwtpid',AD_ID:'pwtsid',PRICE_BUCKET:'pwtecp',SIZE:'pwtsz',DEAL:'pwtdeal',DEAL_ID:'pwtdid',SOURCE:'',FORMAT:'pwtplt',UUID:'pwtuuid',CACHE_ID:'pwtcid',CACHE_HOST:'pwtcurl',ADOMAIN:'pwtadomain'}"))
       .pipe(gulp.dest('build/'));
   } else {
     // console.log("We need to use Prebid keys, so changing targeting keys in PrebidJS config");
     return gulp.src('build/*/' + getBundleName(), { 'allowEmpty': true })
-      .pipe(replace(/'%%TG_KEYS%%'/g, "{BIDDER:'hb_bidder',AD_ID:'hb_adid',PRICE_BUCKET:'hb_pb',SIZE:'hb_size',DEAL:'hb_deal',SOURCE:'hb_source',FORMAT:'hb_format',UUID:'hb_uuid',CACHE_ID:'hb_cache_id',CACHE_HOST:'hb_cache_host',ADOMAIN:'hb_adomain',ACAT:'hb_acat',CRID:'hb_crid',DSP:'hb_dsp'}"))
+      .pipe(replace(/['"]%%TG_KEYS%%['"]/g, "{BIDDER:'hb_bidder',AD_ID:'hb_adid',PRICE_BUCKET:'hb_pb',SIZE:'hb_size',DEAL:'hb_deal',SOURCE:'hb_source',FORMAT:'hb_format',UUID:'hb_uuid',CACHE_ID:'hb_cache_id',CACHE_HOST:'hb_cache_host',ADOMAIN:'hb_adomain',ACAT:'hb_acat',CRID:'hb_crid',DSP:'hb_dsp'}"))
       .pipe(gulp.dest('build/'));
   }
 });
@@ -607,12 +607,12 @@ gulp.task('bundle-native-keys', function() {
   if (usePBJSKeysEnabled) {
     // console.log("We need to use Prebid keys for Native, so changing targeting keys in PrebidJS config");
     return gulp.src('build/*/' + getBundleName(), { 'allowEmpty': true })
-      .pipe(replace(/'%%TG_NATIVE_KEYS%%'/g, "{title:'hb_native_title',body:'hb_native_body',body2:'hb_native_body2',privacyLink:'hb_native_privacy',privacyIcon:'hb_native_privicon',sponsoredBy:'hb_native_brand',image:'hb_native_image',icon:'hb_native_icon',clickUrl:'hb_native_linkurl',displayUrl:'hb_native_displayurl',cta:'hb_native_cta',rating:'hb_native_rating',address:'hb_native_address',downloads:'hb_native_downloads',likes:'hb_native_likes',phone:'hb_native_phone',price:'hb_native_price',salePrice:'hb_native_saleprice',rendererUrl:'hb_renderer_url',adTemplate:'hb_adTemplate'}"))
+      .pipe(replace(/['"]%%TG_NATIVE_KEYS%%['"]/g, "{title:'hb_native_title',body:'hb_native_body',body2:'hb_native_body2',privacyLink:'hb_native_privacy',privacyIcon:'hb_native_privicon',sponsoredBy:'hb_native_brand',image:'hb_native_image',icon:'hb_native_icon',clickUrl:'hb_native_linkurl',displayUrl:'hb_native_displayurl',cta:'hb_native_cta',rating:'hb_native_rating',address:'hb_native_address',downloads:'hb_native_downloads',likes:'hb_native_likes',phone:'hb_native_phone',price:'hb_native_price',salePrice:'hb_native_saleprice',rendererUrl:'hb_renderer_url',adTemplate:'hb_adTemplate'}"))
       .pipe(gulp.dest('build/'));
   } else {
     // console.log("We need to use PWT keys for Native, so changing targeting keys in PrebidJS config");
     return gulp.src('build/*/' + getBundleName(), { 'allowEmpty': true })
-      .pipe(replace(/'%%TG_NATIVE_KEYS%%'/g, "{title:'pwt_native_title',body:'pwt_native_body',body2:'pwt_native_body2',privacyLink:'pwt_native_privacy',privacyIcon:'pwt_native_privicon',sponsoredBy:'pwt_native_brand',image:'pwt_native_image',icon:'pwt_native_icon',clickUrl:'pwt_native_linkurl',displayUrl:'pwt_native_displayurl',cta:'pwt_native_cta',rating:'pwt_native_rating',address:'pwt_native_address',downloads:'pwt_native_downloads',likes:'pwt_native_likes',phone:'pwt_native_phone',price:'pwt_native_price',salePrice:'pwt_native_saleprice',rendererUrl:'pwt_renderer_url',adTemplate:'pwt_adTemplate'}"))
+      .pipe(replace(/['"]%%TG_NATIVE_KEYS%%['"]/g, "{title:'pwt_native_title',body:'pwt_native_body',body2:'pwt_native_body2',privacyLink:'pwt_native_privacy',privacyIcon:'pwt_native_privicon',sponsoredBy:'pwt_native_brand',image:'pwt_native_image',icon:'pwt_native_icon',clickUrl:'pwt_native_linkurl',displayUrl:'pwt_native_displayurl',cta:'pwt_native_cta',rating:'pwt_native_rating',address:'pwt_native_address',downloads:'pwt_native_downloads',likes:'pwt_native_likes',phone:'pwt_native_phone',price:'pwt_native_price',salePrice:'pwt_native_saleprice',rendererUrl:'pwt_renderer_url',adTemplate:'pwt_adTemplate'}"))
       .pipe(gulp.dest('build/'));
   }
 });
