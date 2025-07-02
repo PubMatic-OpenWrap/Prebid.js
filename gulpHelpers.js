3b9161eadc24c5da6970081c6ce8470c5007f738
pm-azhar-mulla <azhar@L2047.local>
2025-07-02 21:46:56 +0530

    Updated gulp file path with functions

--- gulpHelpers.js
+++ gulpHelpers.js
@@ -9,8 +9,8 @@ const gutil = require('gulp-util');
 const submodules = require('./modules/.submodules.json').parentModules;
 
 const MODULE_PATH = './modules';
-export const BUILD_PATH = './build/dist';
-export const DEV_PATH = './build/dev';
+const BUILD_PATH = './build/dist';
+const DEV_PATH = './build/dev';
 const ANALYTICS_PATH = '../analytics';
 
 // get only subdirectories that contain package.json with 'main' property
@@ -25,6 +25,12 @@ function isModuleDirectory(filePath) {
 }
 
 module.exports = {
+  getDevPath: function(){
+    return DEV_PATH;
+  },
+  getBuildPath: function(){
+    return BUILD_PATH;
+  },
   parseBrowserArgs: function (argv) {
     return (argv.browsers) ? argv.browsers.split(',') : [];
   },
--- gulpfile.js
+++ gulpfile.js
@@ -232,7 +232,7 @@ function getModulesListToAddInBanner(modules) {
 }
 
 function gulpBundle(dev) {
-  return bundle(dev, undefined, dev ? helpers.DEV_PATH : helpers.BUILD_PATH).pipe(gulp.dest('build/' + (dev ? 'dev' : 'dist')));
+  return bundle(dev, undefined, dev ? helpers.getDevPath() : helpers.getBuildPath()).pipe(gulp.dest('build/' + (dev ? 'dev' : 'dist')));
 }
 
 function nodeBundle(modules, dev = false) {
