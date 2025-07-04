294e52b53f7f44225d8a4f5c29f8a0295c3ea7fb
pm-azhar-mulla <azhar@L2047.local>
2025-07-04 14:39:01 +0530

    updated webpack for libraries folder

--- webpack.conf.js
+++ webpack.conf.js
@@ -105,7 +105,7 @@ module.exports = {
         }
       })
     ],
-    splitChunks: {
+    splitChunks: (argv._ && argv._.includes('bundle')) ? false : {
       chunks: 'initial',
       minChunks: 1,
       minSize: 0,
