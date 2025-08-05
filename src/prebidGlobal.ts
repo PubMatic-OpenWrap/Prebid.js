5bbf8adf6d984040b71a3e6e4615608230febc05
pm-azhar-mulla <azhar@L2047.local>
2025-08-05 22:29:59 +0530

    Replaced pbjsglobal for namespace clash

--- src/prebidGlobal.ts
+++ src/prebidGlobal.ts
@@ -27,13 +27,12 @@ export interface PrebidJS {
   scheduler?: { yield: () => Promise<void> }
 }
 
-// if $$PREBID_GLOBAL$$ already exists in global document scope, use it, if not, create the object
-// global defination should happen BEFORE imports to avoid global undefined errors.
+// if global variable already exists in global document scope, use it, if not, create the object
+// global definition should happen BEFORE imports to avoid global undefined errors.
 /* eslint-disable */
-if ((window as any).$$PREBID_GLOBAL$$) { console.warn(`Namespace clash happened, with name: ${'window.$$PREBID_GLOBAL$$'}, now you can provide your custom namespace, by creating new profile version in the UI. Existing PWT version details: ${JSON.stringify((window as any)?.PWT?.versionDetails)}`); }
+const globalVarName = getGlobalVarName();
+if ((window as any)[globalVarName]) { console.warn(`Namespace clash happened, with name: ${'window.' + globalVarName}, now you can provide your custom namespace, by creating new profile version in the UI. Existing PWT version details: ${JSON.stringify((window as any)?.PWT?.versionDetails)}`); }
 /* eslint-disable */
-
-// if $$PREBID_GLOBAL$$ already exists in global document scope, use it, if not, create the object
 
 // if the global already exists in global document scope, use it, if not, create the object
 const scope: any = !shouldDefineGlobal() ? {} : window;
