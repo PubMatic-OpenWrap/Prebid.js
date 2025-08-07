23b0d88fab157ae687ea8df40cf5c1d3e2fe8de6
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2025-08-07 17:42:29 +0530

    Sync with latest RTD changes

--- modules/pubmaticRtdProvider.js
+++ modules/pubmaticRtdProvider.js
@@ -134,8 +134,7 @@ const init = (config, _userConsent) => {
  */
 const getBidRequestData = (reqBidsConfigObj, callback) => {
   _ymConfigPromise.then(() => {
-    return pluginManager.executeHook('processBidRequest', reqBidsConfigObj);
-  }).then(() => {
+    pluginManager.executeHook('processBidRequest', reqBidsConfigObj);
     // Apply country information if available
     const country = configJsonManager.country;
     if (country) {
