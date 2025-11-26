341d97bb3e5fe3bd4bdda7fb25f6d74fadff9d90
pm-azhar-mulla <azhar@L2047.local>
2025-11-26 13:56:41 +0530

    used mergDeep for ortb object

--- modules/rtdModule/index.js
+++ modules/rtdModule/index.js
@@ -316,7 +316,7 @@ export const setBidRequestsData = timedAuctionHook('rtd', function setBidRequest
 
   relevantSubModules.forEach(sm => {
     const fpdGuard = guardOrtb2Fragments(reqBidsConfigObj.ortb2Fragments || {}, activityParams(MODULE_TYPE_RTD, sm.name));
-    reqBidsConfigObj.ortb2Fragments = fpdGuard;
+    reqBidsConfigObj.ortb2Fragments = mergeDeep({}, reqBidsConfigObj.ortb2Fragments || {}, fpdGuard);
     sm.getBidRequestData(reqBidsConfigObj, onGetBidRequestDataCallback.bind(sm), sm.config, _userConsent, timeout);
   });
 
