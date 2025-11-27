96a89d4b2d43624a321d93cfe939192ed421a3ad
priyankadeshmane <priyanka.deshmane@pubmatic.com>
2025-11-27 20:46:47 +0530

    Bugfix

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -288,9 +288,9 @@ function copyRequiredBidDetails(bid) {
   ]);
 }
 
-function setBidStatus(bid, args) {
+function setBidStatus(bid, status) {
   if (bid?.status === ERROR && bid?.error?.code === TIMEOUT_ERROR) { return; }
-  switch (args.getStatusCode()) {
+  switch (status) {
     case STATUS.GOOD:
       bid.status = SUCCESS;
       delete bid.error; // it's possible for this to be set by a previous timeout
@@ -734,7 +734,7 @@ const eventHandlers = {
 
     bid.adId = args.adId;
     bid.source = formatSource(bid.source || args.source);
-    setBidStatus(bid, args);
+    setBidStatus(bid, 1);
     const latency = args?.timeToRespond || Date.now() - cache.auctions[args.auctionId].timestamp;
     const auctionTime = cache.auctions[args.auctionId].timeout;
     // Check if latency is greater than auctiontime+150, then log auctiontime+150 to avoid large numbers
