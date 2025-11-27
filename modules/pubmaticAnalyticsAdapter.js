3b09506339f1d17b53c55591c1886bfffc3df41a
pm-azhar-mulla <azhar@L2047.local>
2025-11-27 17:06:22 +0530

    setDebounce adjusted

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -1,5 +1,5 @@
-import { isArray, logError, logWarn, pick, isFn } from '../src/utils.js';
-import adapter from '../libraries/analyticsAdapter/AnalyticsAdapter.js';
+import { _each, isArray, logError, logWarn, pick, isFn, isEmpty } from '../src/utils.js';
+import { default as adapter, setDebounceDelay } from '../libraries/analyticsAdapter/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
 import { BID_STATUS, STATUS, REJECTION_REASON } from '../src/constants.js';
 import { ajax } from '../src/ajax.js';
