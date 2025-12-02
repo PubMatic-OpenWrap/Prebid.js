96bcf15f4daa70f72458503e11ceaecbb9fdb8a5
pm-tanishka-vishwakarma <54441656+pm-tanishka-vishwakarma@users.noreply.github.com>
2025-12-02 12:48:33 +0530

    adding test-case coverage for hour of day implementation

--- test/spec/modules/pubmaticRtdProvider_spec.js
+++ test/spec/modules/pubmaticRtdProvider_spec.js
@@ -315,6 +315,7 @@ describe('Pubmatic RTD Provider', () => {
                 'os',
                 'country',
                 'utm',
+                'hourOfDay',
                 'bidder'
             ]);
 
@@ -363,6 +364,8 @@ describe('Pubmatic RTD Provider', () => {
             expect(result.floors.additionalSchemaFields.os).to.equal(getOs);
             expect(result.floors.additionalSchemaFields.country).to.equal(getCountry);
             expect(result.floors.additionalSchemaFields.utm).to.equal(getUtm);
+            expect(result.floors.additionalSchemaFields.country).to.equal(getCountry);
+            expect(result.floors.additionalSchemaFields.hourOfDay).to.equal(getHourOfDay);
             expect(result.floors.additionalSchemaFields.bidder).to.equal(getBidder);
         });
 
